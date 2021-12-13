12941e3e3c370dd409d6798c4bb3941d0b29415e
Gabriel Asselin <gasselin@c2r3p7.42quebec.com>
2021-12-13 14:48:06 -0500

    CPP01 done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -14,48 +14,66 @@
 #include <string.h>
 #include <fstream>
 
+int	nb_lines(char *argv) {
+	std::string	line;
+	std::ifstream	ifs(argv);
+	int	i = 0;
+
+	while (getline(ifs, line))
+		i++;
+	ifs.close();
+	return i;
+}
+
 std::string	replace_s1(std::string line, std::string s1, std::string s2) {
 	size_t	i;
 
-	i = line.find(s1);
+	i = line.find(s1, 0);
 	while (i != std::string::npos) {
 		line.erase(i, s1.length());
 		line.insert(i, s2);
-		i = line.find(s1, i);
+		i = line.find(s1, i + strlen(s2.c_str()));
 	}
 	return line;
 }
 
 int	main(int argc, char **argv) {
-	std::ofstream	ofs;
-	std::ifstream	ifs;
 	std::string		line;
 
 	if (argc != 4) {
 		std::cout << "Error: 3 arguments needed!" << std::endl;
 		return 0;
 	}
-	ifs.open(argv[1]);
+
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	std::string str[nb_lines(argv[1])];
+
+	std::ifstream	ifs(argv[1]);
 	if (ifs.fail()) {
 		std::cout << "Input file error!" << std::endl;
 		return 0;
 	}
-	ofs.open(strcat(argv[1], ".replace"));
+
+	int i = 0;
+	while (std::getline(ifs, str[i]))
+		i++;
+	ifs.close();
+
+	for (int j = 0; j < i; j++) {
+		if (str[j].find(s1) != std::string::npos) 
+			str[j] = replace_s1(str[j], s1, s2);
+	}
+
+	std::ofstream	ofs(strcat(argv[1], ".replace"));
 	if (ofs.fail()) {
 		std::cout << "Output file error!" << std::endl;
 		return 0;
 	}
-	while (!ifs.eof()) {
-		getline(ifs, line);
-		std::cout << line << std::endl;
-		if (line.find(argv[2]) != std::string::npos)
-			line = replace_s1(line, argv[2], argv[3]);
-		ofs << line;
-		if (ifs.eof())
-			break ;
-		ofs << std::endl;
-	}
+	
+	for (int j = 0; j < i; j++)
+		ofs << str[j] << std::endl;
+
 	ofs.close();
-	ifs.close();
 	return 0;
 }
