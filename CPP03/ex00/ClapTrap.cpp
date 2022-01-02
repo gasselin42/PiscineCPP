ec6e3fa5814db53604eed8078baf439d4df99e8a
gasselin42 <80594358+gasselin42@users.noreply.github.com>
2022-01-02 18:52:43 -0500

    Update ClapTrap.cpp

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -1,1 +1,17 @@
 #include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string _Name) {
+    std::cout << "Constructor of " << _Name << " has been called" << std::endl;
+    Name = _Name;
+    HitPoints = 10;
+    EnergyPoints = 10;
+    Damage = 0;
+}
+
+ClapTrap::~ClapTrap() {
+    std::cout << "Destructor of " << this->Name << " has been called" << std::endl;
+}
+
+void ClapTrap::attack(std::string const & target) {
+
+}
