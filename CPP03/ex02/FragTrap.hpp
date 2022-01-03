8f35f002538255bb8b725fc58804ebdfd3ee0f6a
gasselin42 <80594358+gasselin42@users.noreply.github.com>
2022-01-02 20:57:18 -0500

    Update FragTrap.hpp

--- CPP03/ex02/FragTrap.hpp
+++ CPP03/ex02/FragTrap.hpp
@@ -1,1 +1,16 @@
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAV_HPP
 
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+ public:
+    FragTrap();
+    FragTrap(const std::string & _name);
+    ~FragTrap();
+
+    void highFivesGuys() const;
+    void attack(const std::string & target) const;
+};
+
+#endif
