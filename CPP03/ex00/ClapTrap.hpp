2414039999da37ae80dd34f76ebfa2a3e44fc45e
gasselin42 <80594358+gasselin42@users.noreply.github.com>
2022-01-02 19:19:28 -0500

    Update ClapTrap.hpp

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -6,17 +6,17 @@
 
 class ClapTrap {
  private:
-    std::string const name;
+    std::string name;
     unsigned int hitpoints;
     unsigned int energy;
     unsigned int damage;
 
  public:
     ClapTrap();
-    ClapTrap(std::string _Name);
+    ClapTrap(const std::string & _name);
     ~ClapTrap();
 
-    void attack(std::string const & target) const;
+    void attack(const std::string & target) const;
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
 };
