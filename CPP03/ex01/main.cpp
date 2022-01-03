865b31edaa1d48e130e69405cbb7b7b3793572c3
gasselin42 <80594358+gasselin42@users.noreply.github.com>
2022-01-02 20:49:16 -0500

    Update main.cpp

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -1,1 +1,23 @@
+#include "ScavTrap.hpp"
 
+int main(void) {
+    ClapTrap clap("Mark");
+    ScavTrap scav("Mario");
+
+    clap.attack("John");
+    clap.takeDamage(5);
+    clap.beRepaired(3);
+    clap.beRepaired(10);
+    clap.takeDamage(20);
+
+    std::cout << std::endl;
+
+    scav.attack("Emilio");
+    scav.takeDamage(70);
+    scav.beRepaired(15);
+    scav.beRepaired(60);
+    scav.takeDamage(150);
+    scav.guardGate();
+
+    return 0;
+}
