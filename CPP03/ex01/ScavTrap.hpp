85395d5d66687b44a478b930434bd1496ab3d021
gasselin42 <80594358+gasselin42@users.noreply.github.com>
2022-01-02 20:16:58 -0500

    Update ScavTrap.hpp

--- CPP03/ex01/ScavTrap.hpp
+++ CPP03/ex01/ScavTrap.hpp
@@ -4,7 +4,13 @@
 #include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap {
- 
+ public:
+    ScavTrap();
+    ScavTrap(const std::string & _name);
+    ~ScavTrap();
+
+    void guardGate() const;
+    void attack(const std::string & target) const;
 };
 
 #endif
