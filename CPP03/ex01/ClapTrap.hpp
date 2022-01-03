a9682a0f422d0ada342754faba61e454927356c7
gasselin42 <80594358+gasselin42@users.noreply.github.com>
2022-01-02 19:56:51 -0500

    Create ClapTrap.hpp

---
+++ CPP03/ex01/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+ private:
+    std::string name;
+    unsigned int hitpoints;
+    unsigned int energy;
+    unsigned int damage;
+
+ public: 
+    ClapTrap();
+    ClapTrap(const std::string & _name);
+    ~ClapTrap();
+
+    void attack(const std::string & target) const;
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+#endif
