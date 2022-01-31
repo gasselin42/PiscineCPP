714bea6e36ce16a8cf73d6fd560276f0a6badf75
Gabriel Asselin <gasselin@c2r3p3.42quebec.com>
2022-01-31 09:50:44 -0500

    oops

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,12 +6,25 @@
 /*   By: gasselin <gasselin@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 15:22:34 by gasselin          #+#    #+#             */
-/*   Updated: 2021/12/14 15:22:57 by gasselin         ###   ########.fr       */
+/*   Updated: 2022/01/31 09:49:52 by gasselin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int	main(void) {
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
 	
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max(a, b) << std::endl;
+	
+	return 0;
 }
