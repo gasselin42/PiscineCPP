afd75395860c53a4f1a98a01497b04fff9dfa9a4
Gabriel Asselin <gasselin@c2r3p4.42quebec.com>
2022-02-03 09:56:44 -0500

    Bureaucrat

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gasselin <gasselin@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 11:06:53 by gasselin          #+#    #+#             */
-/*   Updated: 2022/01/14 11:07:03 by gasselin         ###   ########.fr       */
+/*   Updated: 2022/02/03 09:55:46 by gasselin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,42 @@
 
 int main()
 {
+	std::cout << "Test Grade 0 : ";
     try {
         Bureaucrat c("Exception", 0);
     } catch (std::exception& e) {
-        std::cout << e.what() << "\n";
+        std::cout << e.what();
     }
 
+	std::cout << "Test Grade 151 : ";
     try {
         Bureaucrat c("Exception", 151);
     } catch (std::exception& e) {
-        std::cout << e.what() << "\n";
+        std::cout << e.what() << std::endl;
     }
 
     Bureaucrat a("John", 1);
     Bureaucrat b("Joe", 150);
 
     try {
-        std::cout << a << "\n";
+        std::cout << "Init grade : " << a;
         a.decrementGrade();
-        std::cout << a << "\n";
+        std::cout << "Decrement grade : " << a;
         a.incrementGrade();
-        std::cout << a << std::endl;
+        std::cout << "Increment grade : " << a;
         a.incrementGrade();
     } catch (std::exception& e) {
-        std::cout << e.what() << "\n";
+        std::cout << "Increment grade : " << e.what() << std::endl;
     }
 
     try {
-        std::cout << b << "\n";
+        std::cout << "Init grade : " << b;
         b.incrementGrade();
-        std::cout << b << "\n";
+        std::cout << "Increment grade : " << b;
         b.decrementGrade();
-        std::cout << b << std::endl;
+        std::cout << "Decrement grade : " << b;
         b.decrementGrade();
     } catch (std::exception& e) {
-        std::cout << e.what() << "\n";
+        std::cout << "Decrement grade : " << e.what() << std::endl;
     }
 }
